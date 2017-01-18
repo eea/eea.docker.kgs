0b61151e56b3470b84ea2a17ea5350f047fb2fbe
Alin Voinea <alin@eaudeweb.ro>
2017-01-18 21:31:18 +0200

    Revert "Revert"
    This reverts commit e5f942be0e52c398ce4dde55abcb00f5137e9889.

--- kgs-entrypoint.sh
+++ kgs-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# Fix permissions
+# Fix permissions for external volumes
 #
 mkdir -vp /data/downloads/pdf
 mkdir -vp /data/downloads/tmp
-chown -vR plone:plone /data/downloads /plone
+chown -vR plone:plone /data/downloads
 
 exec gosu plone /plone-entrypoint.sh "$@"
