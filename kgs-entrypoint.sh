7a734f5518a989267a3d62f0dc0382d306a270b2
Alin Voinea <alin@eaudeweb.ro>
2017-01-19 13:20:05 +0200

    Fix permissions on /plone/instance

--- kgs-entrypoint.sh
+++ kgs-entrypoint.sh
@@ -4,6 +4,6 @@
 #
 mkdir -vp /data/downloads/pdf
 mkdir -vp /data/downloads/tmp
-chown -vR plone:plone /data/downloads
+chown -vR plone:plone /data/downloads /plone/instance
 
 exec gosu plone /plone-entrypoint.sh "$@"
