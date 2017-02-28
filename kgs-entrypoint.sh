467bf18a2cd993b8d6348aa0f12aa71f94656158
Alin Voinea <alin@eaudeweb.ro>
2017-02-28 17:03:31 +0200

    Fix permissions at buildout level

--- kgs-entrypoint.sh
+++ kgs-entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-#
-# Fix permissions for external volumes
-#
-mkdir -vp /data/downloads/pdf
-mkdir -vp /data/downloads/tmp
-chown -v plone:plone /plone/instance
-chown -v plone:plone /data/downloads
-chown -v plone:plone /data/downloads/pdf
-chown -v plone:plone /data/downloads/tmp
+
+echo "Fixing permissions for external /data volumes"
+mkdir -vp /data/downloads/pdf /data/downloads/tmp
+chown -v plone:plone /data/downloads /data/downloads/pdf /data/downloads/tmp
 
 exec gosu plone /plone-entrypoint.sh "$@"
