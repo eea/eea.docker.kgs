b60b1b1f8feb4fec41890404418c7f6d64e7a8bc
Alin Voinea <alin@eaudeweb.ro>
2017-02-28 11:51:07 +0200

    Update kgs-entrypoint.sh

--- kgs-entrypoint.sh
+++ kgs-entrypoint.sh
@@ -5,8 +5,8 @@
 mkdir -vp /data/downloads/pdf
 mkdir -vp /data/downloads/tmp
 chown -v plone:plone /plone/instance
-chown -v /data/downloads
-chown -v /data/downloads/pdf
-chown -v /data/downloads/tmp
+chown -v plone:plone /data/downloads
+chown -v plone:plone /data/downloads/pdf
+chown -v plone:plone /data/downloads/tmp
 
 exec gosu plone /plone-entrypoint.sh "$@"
