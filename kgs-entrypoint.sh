0c7a22bc123e9f9e89005fa94ee51905e57458df
Alin Voinea <alin.voinea@eaudeweb.ro>
2017-09-26 16:24:13 +0300

    Refs #88228 - Fix src/ permissions

--- kgs-entrypoint.sh
+++ kgs-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Fixing permissions for external /data volumes"
-mkdir -vp /data/downloads/pdf /data/downloads/tmp /data/suggestions
-chown -v plone:plone /data/downloads /data/downloads/pdf /data/downloads/tmp /data/suggestions
+mkdir -vp /data/downloads/pdf /data/downloads/tmp /data/suggestions /plone/instance/src
+chown -v plone:plone /data/downloads /data/downloads/pdf /data/downloads/tmp /data/suggestions /plone/instance/src
 
 exec gosu plone /plone-entrypoint.sh "$@"
