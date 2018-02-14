a75f227c0e83d5aebd27ddfa0ce1c202194198c8
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-02-14 10:01:44 +0200

    Use existing /data/log volume for logging

--- src/docker/kgs-entrypoint.sh
+++ src/docker/kgs-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Fixing permissions for external /data volumes"
-mkdir -vp /data/logs /data/downloads/pdf /data/downloads/tmp /data/suggestions /plone/instance/src
-chown -v plone:plone /data/logs /data/downloads /data/downloads/pdf /data/downloads/tmp /data/suggestions /plone/instance/src
+mkdir -vp /data/log /data/downloads/pdf /data/downloads/tmp /data/suggestions /plone/instance/src
+chown -v plone:plone /data/log /data/downloads /data/downloads/pdf /data/downloads/tmp /data/suggestions /plone/instance/src
 
 exec gosu plone /plone-entrypoint.sh "$@"
